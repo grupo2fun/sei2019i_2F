3ce210dc05aad2c271cb0513781c4e66c3413ac0
juansevargasc <jsvargasc@unal.edu.co>
2019-07-02 14:06:26 -0500

    actualizacion martes

--- code/app/src/main/java/com/example/ahorcado1/MainActivity.java
+++ code/app/src/main/java/com/example/ahorcado1/MainActivity.java
@@ -4,9 +4,10 @@ import android.os.StrictMode;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-import com.example.ahorcado1.dataAccess.database.Database;
-import com.example.ahorcado1.dataAccess.models.User;
-import com.example.ahorcado1.dataAccess.repositories.UserRepository;
+import com.example.ahorcado1.DataAccess.models.User;
+import com.example.ahorcado1.DataAccess.repositories.UserRepository;
+import com.example.ahorcado1.DataAccess.database.Database;
+
 
 public class MainActivity extends AppCompatActivity
 {
@@ -14,12 +15,15 @@ public class MainActivity extends AppCompatActivity
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
+        //Layout
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         //For network connections in main thread
         StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
         StrictMode.setThreadPolicy(policy);
+        //Database
         Database database = new Database();
+        //New User
         User newUser = new User("Ivan321","F9 Maurice","0000");
         UserRepository userRepo = new UserRepository(database.connection);
         userRepo.create(newUser);
---
+++ code/app/src/main/java/com/example/ahorcado1/loginUserActivity.java
@@ -0,0 +1,16 @@
+package com.example.ahorcado1;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class loginUserActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login_user);
+
+        
+    }
+}
