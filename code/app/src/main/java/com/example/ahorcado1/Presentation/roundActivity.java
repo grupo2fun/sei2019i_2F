cf7395bed82bf4c73944a9077ce37f95e1bc6801
juansevargasc <jsvargasc@unal.edu.co>
2019-07-11 09:07:05 -0500

    ajustes

--- code/app/src/main/java/com/example/ahorcado1/Presentation/roundActivity.java
+++ code/app/src/main/java/com/example/ahorcado1/Presentation/roundActivity.java
@@ -29,36 +29,46 @@ public class roundActivity extends AppCompatActivity {
         final TextView impresion = (TextView) findViewById(R.id.impresion);
         final char[] letras = palabra.toCharArray();
         final char[] letra = palabra.toCharArray();
-        for (int i=0; i<=letras.length;i++){
+        //Convierte letras[] en _
+        for (int i=0; i <= letras.length; i++)
+        {
             letras [i]= '_' ;
         }
+        //Imprime los caracteres _ en impresión (textView)
         String Poculta = String.valueOf(letras);
         impresion.setText(Poculta);
 
+        //Crea la máscara con el tamaño de la palabra
         final boolean[] mask = new boolean[palabra.length()];
         Arrays.fill(mask, Boolean.FALSE);
 
         /*ingresa la letra*/
         EditText e1;
-        e1=(EditText) findViewById(R.id.Tletra);
+        e1 = (EditText) findViewById(R.id.Tletra);
+        //Toma el primer caracter del string
         final char ch = e1.getText().charAt(0);
+        //Botón
         Button IngresrarLetra;
-        IngresrarLetra=(Button) findViewById(R.id.BLetra);
+        IngresrarLetra = (Button) findViewById(R.id.BLetra);
 
         /*imprime la letra cuanto se pincha el boton*/
         IngresrarLetra.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                boolean isTrue;
-                for (int i=0; i<=mask.length;i++){
+                boolean isTrue = true;
+                for (int i=0; i <= mask.length; i++)
+                {
                     if(mask[i]=false){ isTrue=false;break;}
-                    else isTrue=true;
+                    else isTrue = true;
                 }
-                while (isTrue=true){
-                    boolean[] s1=gameController.guess(palabra,mask,ch);
-                    for (int i=0; i<=s1.length;i++){
-                        if(s1[i]=true){
-                            letras[i]=letra[i];
+                while (isTrue == true)
+                {
+                    boolean[] s1 = gameController.guess(palabra, mask, ch);
+                    for (int i=0; i<= s1.length; i++)
+                    {
+                        if(s1[i] == true)
+                        {
+                            letras[i] = letra[i];
                         }
                     }
                     String Poculta = String.valueOf(letras);
