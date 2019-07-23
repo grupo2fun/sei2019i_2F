a9efae1f16024d8bfc8c7b1c497843751e47f3dd
juansevargasc <jsvargasc@unal.edu.co>
2019-07-22 23:08:31 -0500

    adminActivity y dos xmls

--- code/app/src/main/java/com/example/ahorcado1/DataAccess/database/Database.java
+++ code/app/src/main/java/com/example/ahorcado1/DataAccess/database/Database.java
@@ -14,7 +14,7 @@ import java.sql.SQLException;
 
 public class Database {
 
-    private static final String HOST = "jdbc:mysql://10.203.155.45:3306/";        //Direccion ip de la base
+    private static final String HOST = "jdbc:mysql://192.168.0.10:3306/";        //Direccion ip de la base
 
     private static final String DB_NAME = "db_hangman";
     private static final String CONNECTION = HOST + DB_NAME;
